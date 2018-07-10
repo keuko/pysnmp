6759b2fbd1b89f09886fc37aec146e6b0de81f85
Ilya Etingof <etingof@gmail.com>
2018-07-10 22:19:41 +0200

    Add PySnmpError.cause attribute
    To convey parent exception information on re-raise

--- pysnmp/error.py
+++ pysnmp/error.py
@@ -5,6 +5,10 @@
 # License: http://snmplabs.com/pysnmp/license.html
 #
 
+import sys
+
 
 class PySnmpError(Exception):
-    pass
+    def __init__(self, message):
+        self.cause = sys.exc_info()
+        Exception.__init__(message + ', caused by ' + self.cause[1])
